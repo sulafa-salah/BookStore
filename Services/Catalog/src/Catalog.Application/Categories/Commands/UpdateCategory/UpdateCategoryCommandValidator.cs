46d496d4435da6403d33de3b1ee28f79dfe6c4ba
sulafa-salah <82936046+sulafa-salah@users.noreply.github.com>
2025-10-06 22:03:17 +0300

    feat(category): update category (command/handler/validator + PUT endpoint) (#4)
    * feat(category): add UpdateCategory command/handler/validator and repo ExistsByNameExcludingIdAsync
    
    * feat(api): PUT /api/categories/{categoryId}

---
+++ Services/Catalog/src/Catalog.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Categories.Commands.UpdateCategory;
+    public  class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+    {
+        public UpdateCategoryCommandValidator()
+        {
+      
+        RuleFor(x => x.Name)
+          .NotEmpty().WithMessage("Name is required.")
+           .MinimumLength(2).WithMessage("Name must not less than 2 characters.")
+          .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+        RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+    }
+    }
---
+++ Services/Catalog/src/Catalog.Contracts/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Contracts.Categories;
+    public  record UpdateCategoryRequest(string Name, string Description, bool IsActive);
