6744e7727a80d931bb0bf8edb74d9a64940541d3
Sulafa <sulafa.ssm@gmail.com>
2025-10-06 18:49:07 +0300

    feat(api): GET /api/categories (paged, search, sort) + contracts

---
+++ Services/Catalog/src/Catalog.Api/Mappings/PagingMappings.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Common.Models;
+using Catalog.Contracts.Common;
+
+
+
+namespace Catalog.Application.Common.Mappings;
+
+    public static class PagingMappings
+    {
+        public static PagedResponse<TOut> ToResponse<TIn, TOut>(
+            this PagedResult<TIn> src,
+            Func<TIn, TOut> projector)
+        {
+            var items = src.Items.Select(projector).ToList();
+
+            return new PagedResponse<TOut>(
+                Items: items,
+                TotalCount: src.TotalCount,
+                PageNumber: src.PageNumber,
+                PageSize: src.PageSize,
+                TotalPages: src.TotalPages,
+                HasPrevious: src.HasPrevious,
+                HasNext: src.HasNext
+            );
+        }
+    }
---
+++ Services/Catalog/src/Catalog.Contracts/Categories/GetCategoriesRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Contracts.Categories;
+    public  record GetCategoriesRequest(
+     int PageNumber = 1,
+     int PageSize = 10,
+     string? Search = null,
+     string? SortBy = "name",
+     string? SortDir = "asc");
