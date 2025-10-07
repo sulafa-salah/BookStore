6a94363f2ab1a62810a460db9008bdd1ff5168f8
Sulafa <sulafa.ssm@gmail.com>
2025-10-07 17:58:11 +0300

    test(common): add factories and constants for tests

---
+++ Services/Catalog/tests/TestCommon/TestConstants/Constants.Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCommon.TestConstants;
+
+public static partial class Constants
+    {
+        public static class Category
+        {
+            public static readonly Guid Id = Guid.NewGuid();
+
+            public const string Name = "Fiction";
+        public const string Description = "All fiction";
+    }
+    }
