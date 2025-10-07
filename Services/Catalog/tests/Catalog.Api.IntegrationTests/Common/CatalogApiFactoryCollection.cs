f9c7710209b73705104005ed6469ac73dc14c0ee
Sulafa <sulafa.ssm@gmail.com>
2025-10-07 18:02:16 +0300

    test(integration): add CategoriesController CreateCategory API test + ApiFactory + ResetDatabase

---
+++ Services/Catalog/tests/Catalog.Api.IntegrationTests/Common/CatalogApiFactoryCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.IntegrationTests.Common;
+
+
+    [CollectionDefinition(CollectionName)]
+    public class CatalogApiFactoryCollection : ICollectionFixture<CatalogApiFactory>
+    {
+        public const string CollectionName = "CatalogApiFactoryCollection";
+    }
---
+++ Services/Catalog/tests/Catalog.Api.IntegrationTests/Common/TestDatabase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Catalog.Infrastructure.Persistence;
+
+
+
+namespace Catalog.Api.IntegrationTests.Common;
+/// <summary>
+/// We're using SQLite so no need to spin an actual database.
+/// </summary>
+public class SqliteTestDatabase : IDisposable
+{
+    public SqliteConnection Connection { get; }
+
+    public static SqliteTestDatabase CreateAndInitialize()
+    {
+        var testDatabase = new SqliteTestDatabase("DataSource=:memory:");
+
+        testDatabase.InitializeDatabase();
+
+        return testDatabase;
+    }
+
+    public void InitializeDatabase()
+    {
+        Connection.Open();
+        var options = new DbContextOptionsBuilder<CatalogDbContext>()
+            .UseSqlite(Connection)
+            .Options;
+
+        var context = new CatalogDbContext(options);
+
+        context.Database.EnsureCreated();
+    }
+
+    public void ResetDatabase()
+    {
+        Connection.Close();
+
+        InitializeDatabase();
+    }
+
+    private SqliteTestDatabase(string connectionString)
+    {
+        Connection = new SqliteConnection(connectionString);
+    }
+
+    public void Dispose()
+    {
+        Connection.Close();
+    }
+}
