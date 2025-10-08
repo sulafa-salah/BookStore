dee6d5f297c37f4910c2995b9dc03cc2fec9ce0a
Sulafa <sulafa.ssm@gmail.com>
2025-10-08 16:08:17 +0300

    Feat(Author): add UpdateAuthor command, handler, request contract, and controller endpoint

--- Services/Catalog/src/Catalog.Domain/AuthorAggregate/Author.cs
+++ Services/Catalog/src/Catalog.Domain/AuthorAggregate/Author.cs
@@ -23,6 +23,8 @@ namespace Catalog.Domain.AuthorAggregate;
     public Author(string name, string bio, bool isActive = true, Guid? id = null) : base(id ?? Guid.NewGuid())
     { Name = name; Biography = bio; IsActive = isActive; }
 
+    public void Update(string name, string bio, bool isActive =true)
+    { Name = name; Biography = bio; IsActive = isActive; }
     public void Activate() => IsActive = true;
     public void Deactivate() => IsActive = false;
 }
