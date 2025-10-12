fe5a0f1d19fabbf9d4c19c0f961c5115c425d365
Sulafa <sulafa.ssm@gmail.com>
2025-10-12 21:54:42 +0300

    feat(shared-kernel): add IIntegrationEvent base and structure for integration events

---
+++ Shared/SharedKernel/IntegrationEvents/Catalog/BookCreatedIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.IntegrationEvents.Catalog;
+    public record BookCreatedIntegrationEvent(string Name,Guid BookId,string ISBN) : IIntegrationEvent;
+    
+
